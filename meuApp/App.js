e3ecffffe554f53e2d0bbb77ebddb02b9f5a0f34
Samuel Alves Chagas <samuelchagasalves@gmail.com>
2021-03-23 01:18:41 -0300

    ADD.: Learning how to use react state

--- meuApp/App.js
+++ meuApp/App.js
@@ -1,36 +1,30 @@
 import React, {Component} from 'react';
-import {View, Text, Image} from 'react-native';
-
-let img = 'https://sujeitoprogramador.com/steve.png';
+import {View, Text, Button} from 'react-native';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      nome: '',
+    };
+
+    this.entrar = this.entrar.bind(this);
+  }
+
+  entrar(nome) {
+    this.setState({
+      nome: nome,
+    });
+  }
   render() {
-    let nome = 'Samuel';
     return (
-      <View>
-        <Text>Olá Mundo!</Text>
-        <Text>Meu primeiro App!</Text>
-        <Text style={{color: 'red', fontSize: 25, margin: 15}}>
-          Sujeito programador
+      <View style={{marginTop: 20}}>
+        <Button title="Entrar" onPress={() => this.entrar('Samuel')} />
+        <Text style={{fontSize: 23, color: 'red', textAlign: 'center'}}>
+          {this.state.nome}
         </Text>
-        <Text style={{fontSize: 30}}>{nome}</Text>
-        <Jobs largura={500} altura={200} fulano="Steve Jobs"></Jobs>
       </View>
     );
   }
 }
 export default App;
-
-class Jobs extends Component {
-  render() {
-    return (
-      <View>
-        <Image
-          source={{uri: img}}
-          style={{width: this.props.largura, height: this.props.altura}}
-        />
-        <Text>{this.props.fulano}</Text>
-      </View>
-    );
-  }
-}
