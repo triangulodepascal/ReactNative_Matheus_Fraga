4cd5fc841356c23b6f84a53b12ac786564035438
Samuel Alves Chagas <samuelchagasalves@gmail.com>
2021-03-23 01:34:05 -0300

    ADD.: Learning how to work with dynamic and fixed sizes.

--- meuApp/App.js
+++ meuApp/App.js
@@ -1,31 +1,27 @@
 import React, {Component} from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import {View, Text} from 'react-native';
 
 class App extends Component {
   render() {
     return (
-      <View style={styles.area}>
-        <Text style={[styles.textoPrincipal, styles.alinhaTexto]}>
-          Eu sou texto 1
-        </Text>
-        <Text style={styles.alinhaTexto}> Eu sou texto 2</Text>
-        <Text> Eu sou texto 3</Text>
-        <Text style={styles.textoPrincipal}> Eu sou texto 4</Text>
+      <View style={{flex: 1, backgroundColor: '#222'}}>
+        <View
+          style={{
+            height: 65,
+            backgroundColor: '#222',
+          }}></View>
+        <View
+          style={{
+            flex: 2,
+            backgroundColor: 'white',
+          }}></View>
+        <View
+          style={{
+            height: 65,
+            backgroundColor: '#222',
+          }}></View>
       </View>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  area: {
-    marginTop: 40,
-  },
-  textoPrincipal: {
-    fontSize: 25,
-    color: 'red',
-  },
-  alinhaTexto: {
-    textAlign: 'center',
-  },
-});
 export default App;
