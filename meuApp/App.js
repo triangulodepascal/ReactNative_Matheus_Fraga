0ab27b359f0edea43a66423e8c18e33bee342dae
Samuel Alves Chagas <samuelchagasalves@gmail.com>
2021-03-24 01:45:03 -0300

    ADD.: Learning how to receive data from user

--- meuApp/App.js
+++ meuApp/App.js
@@ -1,40 +1,56 @@
 import React, { Component } from "react";
-import { View } from "react-native";
+import { View, Text, StyleSheet, TextInput } from "react-native";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      nome: "",
+    };
+
+    this.pegaNome = this.pegaNome.bind(this);
+  }
+
+  pegaNome(texto) {
+    if (texto.length > 0) {
+      this.setState({ nome: "Bem vindo: " + texto });
+    } else {
+      this.setState({ nome: "" });
+    }
+  }
+
   render() {
     return (
-      <View
-        style={{
-          flex: 1,
-          flexDirection: "column",
-          alignItems: "center",
-          justifyContent: "center",
-        }}
-      >
-        <View
-          style={{
-            width: 50,
-            height: 50,
-            backgroundColor: "green",
-          }}
-        ></View>
-        <View
-          style={{
-            width: 50,
-            height: 50,
-            backgroundColor: "red",
-          }}
-        ></View>
-        <View
-          style={{
-            width: 50,
-            height: 50,
-            backgroundColor: "yellow",
-          }}
-        ></View>
+      <View>
+        <TextInput
+          style={styles.input}
+          placeholder="Digite seu nome"
+          // Para cada mudança no texto do input, chama-se a função onChangeText que cria a responsividade
+          onChangeText={this.pegaNome}
+        ></TextInput>
+
+        <Text style={styles.texto}>{this.state.nome}</Text>
       </View>
     );
   }
 }
 export default App;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  input: {
+    height: 45,
+    borderWidth: 1,
+    borderColor: "#222",
+    marginTop: 30,
+    marginHorizontal: 10,
+    fontSize: 20,
+    padding: 10,
+  },
+  texto: {
+    textAlign: "center",
+    fontSize: 25,
+  },
+});
