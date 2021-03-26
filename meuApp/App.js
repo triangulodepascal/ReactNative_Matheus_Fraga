3a055733c2b2299f2e47a7cd19ec005457926002
Samuel Alves Chagas <samuelchagasalves@gmail.com>
2021-03-26 01:38:45 -0300

    NEW.: Learning how to use scroll view

--- meuApp/App.js
+++ meuApp/App.js
@@ -1,38 +1,20 @@
 import React, { Component } from "react";
-import { View, Text, StyleSheet, TextInput, Button } from "react-native";
+import { View, StyleSheet, ScrollView } from "react-native";
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      nome: "",
-      input: "",
-    };
-
-    this.entrar = this.entrar.bind(this);
-  }
-
-  entrar() {
-    if (this.state.input == "") {
-      alert("Digite seu nome!");
-      this.setState({ nome: "" });
-    } else {
-      this.setState({ nome: "Bem vindo: " + this.state.input });
-    }
-  }
-
   render() {
     return (
-      <View>
-        <TextInput
-          style={styles.input}
-          placeholder="Digite seu nome"
-          // Para cada mudança no texto do input, chama-se a função onChangeText que cria a responsividade
-          onChangeText={(texto) => this.setState({ input: texto })}
-        ></TextInput>
-
-        <Button title="Entrar" onPress={this.entrar}></Button>
-        <Text style={styles.texto}>{this.state.nome}</Text>
+      <View style={styles.container}>
+        <ScrollView
+          horizontal={true}
+          scrollEnabled={true}
+          showsHorizontalScrollIndicator={false}
+        >
+          <View style={styles.box1}></View>
+          <View style={styles.box2}></View>
+          <View style={styles.box3}></View>
+          <View style={styles.box4}></View>
+        </ScrollView>
       </View>
     );
   }
@@ -43,17 +25,24 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
-  input: {
-    height: 45,
-    borderWidth: 1,
-    borderColor: "#222",
-    marginTop: 30,
-    marginHorizontal: 10,
-    fontSize: 20,
-    padding: 10,
+  box1: {
+    backgroundColor: "red",
+    height: 250,
+    width: 150,
   },
-  texto: {
-    textAlign: "center",
-    fontSize: 25,
+  box2: {
+    backgroundColor: "green",
+    height: 250,
+    width: 150,
+  },
+  box3: {
+    backgroundColor: "yellow",
+    height: 250,
+    width: 150,
+  },
+  box4: {
+    backgroundColor: "blue",
+    height: 250,
+    width: 150,
   },
 });
