fca71c144fd6bfaf185f1788683ff59edaf8edaa
Samuel Alves Chagas <samuelchagasalves@gmail.com>
2021-03-26 02:00:27 -0300

    NEW.: Learning how to work with flatList

--- meuApp/App.js
+++ meuApp/App.js
@@ -1,48 +1,76 @@
 import React, { Component } from "react";
-import { View, StyleSheet, ScrollView } from "react-native";
+import { View, StyleSheet, FlatList, Text } from "react-native";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      feed: [
+        {
+          id: "1",
+          nome: "Samuel",
+          idade: 19,
+          email: "samuelchagasalves@gmail.com",
+        },
+        {
+          id: "2",
+          nome: "Sarah",
+          idade: 18,
+          email: "sarahchagasalves@gmail.com",
+        },
+        {
+          id: "3",
+          nome: "Maira",
+          idade: 21,
+          email: "mairachagasalves@gmail.com",
+        },
+        {
+          id: "4",
+          nome: "Matheus",
+          idade: 26,
+          email: "matheuschagasalves@gmail.com",
+        },
+      ],
+    };
+  }
   render() {
     return (
       <View style={styles.container}>
-        <ScrollView
-          horizontal={true}
-          scrollEnabled={true}
-          showsHorizontalScrollIndicator={false}
-        >
-          <View style={styles.box1}></View>
-          <View style={styles.box2}></View>
-          <View style={styles.box3}></View>
-          <View style={styles.box4}></View>
-        </ScrollView>
+        <FlatList
+          data={this.state.feed}
+          keyExtractor={(item) => item.id}
+          renderItem={({ item }) => <Pessoa data={item} />}
+        ></FlatList>
       </View>
     );
   }
 }
-export default App;
+
+class Pessoa extends Component {
+  render() {
+    return (
+      <View style={styles.areaPessoa}>
+        <Text style={styles.textoPessoa}>Nome: {this.props.data.nome}</Text>
+        <Text style={styles.textoPessoa}>Idade: {this.props.data.idade}</Text>
+        <Text style={styles.textoPessoa}>Email: {this.props.data.email}</Text>
+      </View>
+    );
+  }
+}
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
-  box1: {
-    backgroundColor: "red",
-    height: 250,
-    width: 150,
+  areaPessoa: {
+    backgroundColor: "#222",
+    height: 200,
+    marginTop: 25,
   },
-  box2: {
-    backgroundColor: "green",
-    height: 250,
-    width: 150,
-  },
-  box3: {
-    backgroundColor: "yellow",
-    height: 250,
-    width: 150,
-  },
-  box4: {
-    backgroundColor: "blue",
-    height: 250,
-    width: 150,
+  textoPessoa: {
+    color: "#FFF",
+    fontSize: 20,
   },
 });
+
+export default App;
