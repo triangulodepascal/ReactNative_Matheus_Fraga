61d44f275591d989cd95a91de9ea508e82fe3237
Samuel Alves Chagas <samuelchagasalves@gmail.com>
2021-03-26 02:06:41 -0300

    MOD.: Learning how to structure components in different folders

--- meuApp/App.js
+++ meuApp/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
-import { View, StyleSheet, FlatList, Text } from "react-native";
+import Pessoa from "./src/Pessoas";
+import { View, StyleSheet, FlatList } from "react-native";
 
 class App extends Component {
   constructor(props) {
@@ -46,30 +47,9 @@ class App extends Component {
   }
 }
 
-class Pessoa extends Component {
-  render() {
-    return (
-      <View style={styles.areaPessoa}>
-        <Text style={styles.textoPessoa}>Nome: {this.props.data.nome}</Text>
-        <Text style={styles.textoPessoa}>Idade: {this.props.data.idade}</Text>
-        <Text style={styles.textoPessoa}>Email: {this.props.data.email}</Text>
-      </View>
-    );
-  }
-}
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-  },
-  areaPessoa: {
-    backgroundColor: "#222",
-    height: 200,
-    marginTop: 25,
-  },
-  textoPessoa: {
-    color: "#FFF",
-    fontSize: 20,
   },
 });
 
