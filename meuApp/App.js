4af96c3b75bbb28439d1f2b289fbaccc3b52fd29
Samuel Alves Chagas <samuelchagasalves@gmail.com>
2021-03-28 23:43:18 -0300

    NEW: Learning how to use Slider

--- meuApp/App.js
+++ meuApp/App.js
@@ -1,43 +1,28 @@
 import React, { Component } from "react";
 import { View, Text, StyleSheet } from "react-native";
-import { Picker } from "@react-native-community/picker";
+import Slider from "@react-native-community/slider";
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      pizza: 0,
-      pizzas: [
-        { key: 1, nome: "Strogonoff", valor: 35.9 },
-        { key: 2, nome: "Calabresa", valor: 59 },
-        { key: 3, nome: "Quatro queijos", valor: 37 },
-        { key: 4, nome: "Brigadeiro", valor: 25.7 },
-      ],
+      valor: 0,
     };
   }
   render() {
-    let pizzasItem = this.state.pizzas.map((value, key) => {
-      return <Picker.Item key={key} value={key} label={value.nome} />;
-    });
-
     return (
       <View style={styles.container}>
-        <Text style={styles.logo}>Menu pizza</Text>
-
-        <Picker
-          selectedValue={this.state.pizza}
-          onValueChange={(itemValue, itemIndex) =>
-            this.setState({ pizza: itemValue })
+        <Slider
+          minimumValue={0}
+          maximumValue={100}
+          onValueChange={(valorSelecionado) =>
+            this.setState({ valor: valorSelecionado })
           }
-        >
-          {pizzasItem}
-        </Picker>
-
-        <Text style={styles.pizzas}>
-          Voce escolheu: {this.state.pizzas[this.state.pizza].nome}
-        </Text>
-        <Text style={styles.pizzas}>
-          R$: {this.state.pizzas[this.state.pizza].valor.toFixed(2)}
+          value={this.state.valor}
+        ></Slider>
+        <Text style={{ textAlign: "center", fontSize: 30 }}>
+          {" "}
+          {this.state.valor.toFixed(0)}
         </Text>
       </View>
     );
@@ -48,16 +33,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     marginTop: 25,
-  },
-  logo: {
-    textAlign: "center",
-    fontSize: 28,
-    fontWeight: "bold",
-  },
-  pizzas: {
-    marginTop: 15,
-    fontSize: 25,
-    textAlign: "center",
   },
 });
 
