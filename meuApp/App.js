ac2d58dcf65c4680e7c3885b4384b3632047fd42
Samuel Alves Chagas <samuelchagasalves@gmail.com>
2021-04-08 01:03:23 -0300

    NEW.: Learning how to use modal

--- meuApp/App.js
+++ meuApp/App.js
@@ -1,62 +1,33 @@
 import React, { Component } from "react";
-import {
-  View,
-  Text,
-  StyleSheet,
-  TextInput,
-  TouchableOpacity,
-  Keyboard,
-} from "react-native";
-
-import AsyncStorage from "@react-native-community/async-storage";
+import { View, Text, StyleSheet, Button, Modal } from "react-native";
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      input: "",
-      nome: "",
+      modalVisible: false,
     };
-    this.gravaNome = this.gravaNome.bind(this);
+
+    this.entrar = this.entrar.bind(this);
   }
 
-  async componentDidMount() {
-    await AsyncStorage.getItem("nome").then((value) => {
-      this.setState({ nome: value });
-    });
+  entrar() {
+    this.setState({ modalVisible: true });
   }
 
-  async componentDidUpdate(_, prevState) {
-    const nome = this.state.nome;
-    if (prevState !== nome) {
-      await AsyncStorage.setItem("nome", nome);
-    }
-  }
-
-  gravaNome() {
-    this.setState({
-      nome: this.state.input,
-    });
-    alert("Salvo com sucesso!");
-    Keyboard.dismiss();
-  }
   render() {
     return (
       <View style={styles.container}>
-        <View style={styles.viewInput}>
-          <TextInput
-            style={styles.input}
-            value={this.state.input}
-            onChangeText={(text) => this.setState({ input: text })}
-            underlineColorAndroid="transparent"
-          ></TextInput>
-
-          <TouchableOpacity onPress={this.gravaNome}>
-            <Text style={styles.botao}>+</Text>
-          </TouchableOpacity>
-        </View>
-
-        <Text style={styles.nome}>{this.state.nome}</Text>
+        <Button title="Entrar" onPress={this.entrar}></Button>
+        <Modal animationType="fade" visible={this.state.modalVisible}>
+          <View style={{ backgroundColor: "#292929", flex: 1 }}>
+            <Text style={{ color: "#FFF", fontSize: 28 }}>Seja bem vindo!</Text>
+            <Button
+              title="Sair"
+              onPress={() => this.setState({ modalVisible: false })}
+            ></Button>
+          </View>
+        </Modal>
       </View>
     );
   }
@@ -66,30 +37,9 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     marginTop: 45,
+    justifyContent: "center",
     alignItems: "center",
-  },
-  viewInput: {
-    flexDirection: "row",
-    alignItems: "center",
-  },
-  input: {
-    width: 310,
-    height: 40,
-    borderColor: "#000",
-    borderWidth: 1,
-    padding: 10,
-  },
-  botao: {
-    backgroundColor: "#222",
-    color: "#FFF",
-    height: 40,
-    padding: 10,
-    marginLeft: 4,
-  },
-  nome: {
-    marginTop: 15,
-    fontSize: 30,
-    textAlign: "center",
+    backgroundColor: "#DDD",
   },
 });
 
