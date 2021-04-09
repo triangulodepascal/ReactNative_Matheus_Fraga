71126a94534741ccfb2928a5d5b1a28a565f0c9c
Samuel Alves Chagas <samuelchagasalves@gmail.com>
2021-04-09 01:33:57 -0300

    ADD.: Starting Filmes App

--- meuApp/App.js
+++ meuApp/App.js
@@ -1,45 +1,31 @@
 import React, { Component } from "react";
-import { View, Text, StyleSheet, Button, Modal } from "react-native";
-import Entrar from "./src/Entrar";
+import { View, StyleSheet, FlatList } from "react-native";
+
+import api from "./src/services/api";
+import Filmes from "./src/filmes/index";
+
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      modalVisible: false,
+      filmes: [],
     };
-
-    this.entrar = this.entrar.bind(this);
-    this.sair = this.sair.bind(this);
   }
-
-  entrar() {
-    this.setState({ modalVisible: true });
-  }
-
-  sair(visible) {
-    this.setState({ modalVisible: visible });
+  async componentDidMount() {
+    const response = await api.get("/r-api/?api=filmes");
+    this.setState({
+      filmes: response.data,
+    });
   }
 
   render() {
     return (
       <View style={styles.container}>
-        <Button title="Entrar" onPress={this.entrar}></Button>
-        <Modal
-          transparent={true}
-          animationType="slide"
-          visible={this.state.modalVisible}
-        >
-          <View
-            style={{
-              margin: 15,
-              flex: 1,
-              alignItems: "center",
-              justifyContent: "center",
-            }}
-          >
-            <Entrar fechar={() => this.sair(false)}></Entrar>
-          </View>
-        </Modal>
+        <FlatList
+          data={this.state.filmes}
+          keyExtractor={(item) => item.id.toString()}
+          renderItem={({ item }) => <Filmes data={item} />}
+        ></FlatList>
       </View>
     );
   }
@@ -48,9 +34,9 @@ class App extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#DDD",
+    // justifyContent: "center",
+    // alignItems: "center",
+    // backgroundColor: "#DDD",
   },
 });
 
